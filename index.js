1dbef16915ee256e007cecc7d5e65e53732df7f0
aomeasy <atcharaporn009@gmail.com>
2025-07-25 14:06:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,12 @@ app.post('/save', (req, res) => {
 app.listen(port, () => {
   console.log(`🚀 API running at http://localhost:${port}`);
 });
+
+console.log('🔍 Connecting to MySQL with config:');
+console.log({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+});
