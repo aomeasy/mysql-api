8e4e64f6a6001a77d3b6a6cc59d9650012e36d70
aomeasy <atcharaporn009@gmail.com>
2025-07-26 15:35:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 app.use(express.json());
 
